a2010b118cd8796a612687416ffa12cb5a76e51e
juanonchon <a.d.f.m36dash@gmail.com>
2024-03-04 21:52:07 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Practica1{
+
+    class Program{
+
+    public static void Main (string[] args)
+    {
+        int myNum = 12345;
+        int myNum1 = 12345 % 10;
+        int myNum2 = myNum % myNum1  ;
+
+        Console.WriteLine(myNum);
+        Console.WriteLine(myNum1);
+        Console.WriteLine(myNum2);
+
+
+    }
+    }
+}
